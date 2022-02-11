9fed6b9abd12d5609f7d85974543025ac8599928
ljmesi <37740329+ljmesi@users.noreply.github.com>
2022-02-11 11:33:25 +0100

    Rename dir, s. def. files & use conda for Rmd simg

--- envs/get_cointainers.sh
+++ envs/get_cointainers.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Run me in envs folder to get sinuglarities needed
-# Please adjust paths of simg location in appropriate config_file
+# Run me in envs folder to get needed singularity containers
+# Please adjust paths of simg location in the appropriate config_file
 
 
-sudo singularity build target_variants_python.simg recepies/get_target_variants
-sudo singularity build rmarkdown.simg recepies/Rmarkdown
-sudo singularity build samtools.simg recepies/samtools
+sudo singularity build target_variants_python.simg recipes/get_target_variants
+sudo singularity build rmarkdown.simg recipes/Rmarkdown.def
+sudo singularity build samtools.simg recipes/samtools
 sudo singularity build gatk3.simg docker://broadinstitute/gatk3:3.8-1
-sudo singularity build gatk4.simg docker://broadinstitute/gatk
+sudo singularity build gatk4.simg docker://broadinstitute/gatk:4.2.5.0
